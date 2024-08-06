e31cd4a8a5976ad05da1b52c968476023a707e08
Le-1v <timaleo03@gmail.com>
2024-08-06 16:36:50 +0300

    Add slideholder & fix sound

--- popup.js
+++ popup.js
@@ -1,4 +1,17 @@
-document.getElementById('boost').addEventListener('click', () => {
+const volumeSlider = document.getElementById('volumeSlider');
+const volumeValue = document.getElementById('volumeValue');
+const volumeButton = document.getElementById('boost');
+
+volumeSlider.addEventListener('input', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: boostVolume_v(volumeSlider.value)
+        });
+    });
+});
+
+volumeButton.addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
@@ -7,9 +20,28 @@ document.getElementById('boost').addEventListener('click', () => {
     });
 });
 
+
 function boostVolume() {
     const audioElements = document.querySelectorAll('audio, video');
     audioElements.forEach(element => {
-        element.volume = Math.min(element.volume * 1.5, 1); // Увеличиваем громкость на 50%, но не более 100%
+        const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+        const source = audioCtx.createMediaElementSource(element);
+        const gainNode = audioCtx.createGain();
+        gainNode.gain.value = 100;
+        source.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
     });
+}
+
+function boostVolume_v(value) {
+    const audioElements = document.querySelectorAll('audio, video');
+    audioElements.forEach(element => {
+        const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+        const source = audioCtx.createMediaElementSource(element);
+        const gainNode = audioCtx.createGain();
+        gainNode.gain.value = value;
+        source.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+    });
+    volumeValue.textContent = value;
 }
